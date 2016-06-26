3f495cd51a6b5f749d96fa8eabee2b701130751f
TimidLion <sgm02186@naver.com>
2016-06-26 19:04:09 +0900

    amicable problem solved

--- 4level/amicable.cpp
+++ 4level/amicable.cpp
@@ -1,1 +1,35 @@
 #include<stdio.h>
+
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	for(int i = 2;i <= n; i++)
+	{
+		int sum = 0;
+		for(int d=1;d < i; d++)
+		{
+			if(i%d == 0)
+				sum += d;
+		}
+		int sumOfSum = 0;
+		for(int j = 1; j < sum; j++)
+		{
+			if(sum%j == 0)
+				sumOfSum += j;
+		}
+		if(sumOfSum == i)
+		{
+			if(sum == sumOfSum)
+				continue;
+//		printf("i : %d\n",i);
+//		printf("sum : %d\n",sum);
+//		printf("sum of sum : %d\n",sumOfSum);
+			if(sum > i)
+				printf("%d %d\n",i,sum);
+			else
+				printf("%d %d\n",sum,i);
+			i = sum;
+		}
+	}
+}
