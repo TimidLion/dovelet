d8aa16e64fa63b682d7ba257e57f8906da0117d5
TimidLion <sgm02186@naver.com>
2016-09-07 23:26:04 +0900

    solved by some help

--- 5level/dwarf.cpp
+++ 5level/dwarf.cpp
@@ -5,6 +5,21 @@ int main()
 	int arr[9];
 	for(int i =0; i < 9; i++)
 		scanf("%d",&arr[i]);
-	for(int pick = 0; pick < 9; pick++)
-	
+	int sum = 0;
+	for(int i =0; i < 9; i++)
+		sum += arr[i];
+	int num1,num2;
+	for(int pick = 0; pick < 8; pick++)
+	{
+		for(int pick2 = pick+1; pick2 < 9; pick2++)
+		{
+			int sum2 = sum;
+			sum2 -= (arr[pick] + arr[pick2]);
+			if(sum2 == 100)
+			{	num1 = pick; num2 = pick2;break;	}
+		}
+	}
+	for(int i=0; i < 9; i++)
+		if(i != num1 && i != num2)
+			printf("%d\n",arr[i]);
 }
